1deb6f1b2408def1ccfd9af9572c4fb97c73f7fa
bryson crandall <bryson.crandall@yahoo.com>
2021-09-02 09:53:11 -0700

    custom query for /mercury_users/:user_id that returns permissions

--- server/src/services/recipients-groups-table/recipients-groups-table.hooks.js
+++ server/src/services/recipients-groups-table/recipients-groups-table.hooks.js
@@ -2,20 +2,7 @@ module.exports = {
     before: {
         all: [],
         find: [],
-        get: [
-            context => {
-                const query = context.service.createQuery(context.params);
-
-                query
-                    .select('*').from('recipients_groups_table')
-                    .where({ recipient_id: context.id })
-                    .innerJoin('groups', 'recipients_groups_table.group_id', 'groups.group_id');
-                    //.innerJoin('recipients', 'recipients_groups_table.recipient_id', 'recipients.recipient_id');
-
-                context.params.knex = query;
-                return context;
-            }
-        ],
+        get: [],
         create: [],
         update: [],
         patch: [],
